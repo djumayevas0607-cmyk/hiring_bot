811393edd422cb4591672563bf4c594529a576aa
djumayevas0607-cmyk <djumayevas0607@gmail.com>
2025-09-06 18:56:10 +0500

    Initial commit

---
+++ config.py
@@ -0,0 +1,17 @@
+# config.py
+# Fill in your bot token and main admin id here.
+BOT_TOKEN = "8489619211:AAHQy47GWLsyozrY3208jdDyMLHczwKjzVo"
+
+# ID главного админа
+MAIN_ADMIN = 6106674802
+
+# Initial job types (you can edit freely)
+JOB_TYPES = [
+    "Sotuvchi",
+    "Operator",
+    "Kurier",
+    "Boshqa"
+]
+
+# Where to store media/admin files
+STORAGE_DIR = "./storage"
