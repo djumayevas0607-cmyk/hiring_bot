61156c6d8ac8d1491c7959f07edf92c39d6ef150
djumayevas0607-cmyk <djumayevas0607@gmail.com>
2025-09-22 23:31:22 +0500

    Изменения в проекте

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py
 # Fill in your bot token and main admin id here.
-BOT_TOKEN = "8489619211:AAHQy47GWLsyozrY3208jdDyMLHczwKjzVo"
+BOT_TOKEN = "8489619211:AAElnuUbZRR82RBIfS5WZJ662_3npLuoYKI"
 
 # ID главного админа
 MAIN_ADMIN = 6106674802
