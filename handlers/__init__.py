73eef682e5c0832e09cb07a992809e42be60bd23
djumayevas0607-cmyk <djumayevas0607@gmail.com>
2025-09-23 06:01:14 +0500

    Добавил свою логику в handlers

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,6 +1,10 @@
+# handlers/__init__.py
 from aiogram import Router
-from . import start, other
+
+from .start import router as start_router
+from .other import router as other_router
+# сюда добавляй все остальные файлы, где есть router
 
 router = Router()
-router.include_router(start.router)
-router.include_router(other.router)
+router.include_router(start_router)
+router.include_router(other_router)
