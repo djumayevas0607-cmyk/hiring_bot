e63e39959448e6c8f4733b8e8dec414272a16181
djumayevas0607-cmyk <djumayevas0607@gmail.com>
2026-01-07 00:20:19 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py
 # Fill in your bot token and main admin id here.
-BOT_TOKEN = 8593752690:AAHVOrJIRW3gEm_2Uojwq_xuvbm495A7jog
+BOT_TOKEN = "8593752690:AAHVOrJIRW3gEm_2Uojwq_xuvbm495A7jog"
 
 # ID главного админа
 MAIN_ADMIN = 6106674802
