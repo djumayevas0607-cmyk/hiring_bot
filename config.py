7d45b7d8729541bb6d60c766097b1020979cccc8
djumayevas0607-cmyk <djumayevas0607@gmail.com>
2026-01-31 20:46:59 +0500

    Fix MAIN_ADMIN ID in config.py
    Updated MAIN_ADMIN ID to the correct value.

--- config.py
+++ config.py
@@ -4,7 +4,6 @@ BOT_TOKEN = "8593752690:AAHVOrJIRW3gEm_2Uojwq_xuvbm495A7jog"
 
 # ID главного админа
 MAIN_ADMIN = 6106674802
-MAIN_ADMIN = 1184042910
 
 # Initial job types (you can edit freely)
 JOB_TYPES = [
